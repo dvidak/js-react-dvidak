73fa2f8c8fdaf59f76b1d5f6b5503bee2ebf413e
vidakd <dv49576@fer.hr>
2019-07-10 22:37:38 +0200

    dohvacam letove, hardkodiran token

--- src/index.js
+++ src/index.js
@@ -3,26 +3,48 @@ import ReactDOM from 'react-dom';
 import { useAsync } from 'react-use';
 
 
+
 function getFlights(){
   return fetch('https://flighter-hw7.herokuapp.com/api/flights',{
     headers: {
       'Content-Type': 'application/json',
       'Accept':'application/json',
+      'Authorization':'b2wz5UBXRYZ9poLS6bucVyhe'
+      //authorization dodat
     },
-    credentials: 'cors',
   })
   .then((res) => res.json())
   .catch((err)=> console.log("nije proslo"));
 }
 
+
+
 function HelloWorld() {
   const { loading, value, error } = useAsync(getFlights);
+
+  console.log(value);
 
   return (
     <div>
       <h1>Hello world!</h1>
-      { loading &&  <p>Loading</p>}
-      
+      { loading &&  <p>Loading</p> }
+      <h1>Letovi</h1>
+      <table>
+      { value && value.flights.map ( (f) => 
+            <div>
+              <td> {f.id}</td>
+              <td> {f.name}</td>
+              <td> {f.no_of_seats}</td>
+              <td> {f.base_price}</td>
+              <td> {f.flys_at}</td>
+              <td> {f.lands_at}</td>
+              <td> {f.no_of_booked_seats}</td>
+              <td> {f.company_id}</td>
+              <td> {f.company_name}</td>
+              <td> {f.current_price}</td>
+            </div>
+      )}
+      </table>
     </div>
   );
 }
